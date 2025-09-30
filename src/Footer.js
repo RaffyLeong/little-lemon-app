140c7241dffe4f804e17f5b022fb8d80498f74a2
Raffy <raffy36249@gmail.com>
2025-09-30 21:25:34 +0800

    update html, footer,header, main and nav

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <ul>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/privacy">Privacy Policy</a></li>
+          <li><a href="/terms">Terms of Service</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
